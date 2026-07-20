43c0360be9a98100ff553fe7420553994a302f56
Vallabh <vallabhpanchbhai@2707@gmail>
2026-07-20 15:13:03 +0530

    Started Project

---
+++ src/test/java/com/example/hrm_software/HrmSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrm_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
